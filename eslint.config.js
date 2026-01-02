a1520c7176a0f42922905fb3a117cd07b2ef65ac
Danny Smith <hi@danny.is>
2026-01-02 06:14:58 +0000

    Fix ESLint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier/recommended";
 import obsidianmd from "eslint-plugin-obsidianmd";
 import globals from "globals";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...obsidianmd.configs.recommended,
